d0e36dcafb62ec98ddb96a15957ac76429378f09
at <at@at-ose.de>
2022-08-28 22:47:47 +0200

    pwa

---
+++ sw.js
@@ -0,0 +1,72 @@
+
+console.log('Script loaded!')
+var cacheStorageKey = 'minimal-pwa-8'
+
+var cacheList = [
+  '/',
+  "index.html",
+  "main.css",
+  "e.png",
+  "pwa-fonts.png"
+]
+
+self.addEventListener('install', function(e) {
+  console.log('Cache event!')
+  e.waitUntil(
+    caches.open(cacheStorageKey).then(function(cache) {
+      console.log('Adding to Cache:', cacheList)
+      return cache.addAll(cacheList)
+    }).then(function() {
+      console.log('Skip waiting!')
+      return self.skipWaiting()
+    })
+  )
+})
+
+self.addEventListener('activate', function(e) {
+  console.log('Activate event')
+  e.waitUntil(
+    Promise.all(
+      caches.keys().then(cacheNames => {
+        return cacheNames.map(name => {
+          if (name !== cacheStorageKey) {
+            return caches.delete(name)
+          }
+        })
+      })
+    ).then(() => {
+      console.log('Clients claims.')
+      return self.clients.claim()
+    })
+  )
+})
+
+self.addEventListener('fetch', function(e) {
+  // console.log('Fetch event:', e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      if (response != null) {
+        console.log('Using cache for:', e.request.url)
+        return response
+      }
+      console.log('Fallback to fetch:', e.request.url)
+      return fetch(e.request.url)
+    })
+  )
+});
+
+reg.addEventListener('updatefound', () => {
+
+  const newSW = reg.installing;
+  newSW.addEventListener('statechange', () => {
+  
+    // Check service worker state
+    if (newSW.state === 'installed') {
+        // A new SW is available and installed.
+        // You can update the page directly or better
+        // show a notification to the user to prompt for a page reload 
+        // and inform about the new version available
+        alert("New version available");
+       }
+     });
+    });
